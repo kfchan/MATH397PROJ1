dd580d860ba8449ab4f031d584100a624a62421e
kfchan <kchan@oberlin.edu>
2016-02-24 11:45:46 -0500

    now takes in one file and presents it to user; also removed unnecessary file

--- imageManip.java
+++
@@ -1,8 +0,0 @@
-package project1;
-
-public class imageManip {
-	public static void main(String[] pirateArgs) {
-		Picture canvas = new Picture(1000,1000);
-		canvas.display();
-	}
-}
--- src/project1/imageManip.java
+++ src/project1/imageManip.java
@@ -2,7 +2,10 @@ package project1;
 
 public class imageManip {
 	public static void main(String[] pirateArgs) {
-		Picture canvas = new Picture(500,500);
-		canvas.display();
+		if (pirateArgs.length != 1) {
+			System.err.println("Exactly one file argument needed.");
+		}
+		Picture picture = new Picture("../" + pirateArgs[0], false);
+		picture.display();
 	}
 }
